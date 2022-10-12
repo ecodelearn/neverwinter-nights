419711b2fdd3e27a8684d5ea8caeaa4204105703
ecodelearn <115067428+ecodelearn@users.noreply.github.com>
2022-10-12 17:50:37 -0300

    add comments and improve time after casts

--- galadriel_magias.py
+++ galadriel_magias.py
@@ -29,106 +29,108 @@ pyautogui.FAILSAFE = True
 
 
 pyautogui.moveTo(1249, 116)
-pyautogui.doubleClick()
+pyautogui.click()
+time.sleep(0.5)
+pyautogui.click()
 
 
 # All Shift Buffs Setup
 
 PressKey(shift)
 
-time.sleep(0.3)
-PressKey(f12)
+time.sleep(0.5)
+PressKey(f12) # summons familiar
 ReleaseKey(f12)
-time.sleep(0.3)
+time.sleep(0.5)
 # PressKey(shift)
-time.sleep(0.3)
-PressKey(f11)
+time.sleep(0.5)
+PressKey(f11) # uses Rod of the Ghost
 ReleaseKey(f11)
-time.sleep(0.3)
+time.sleep(0.5)
 # PressKey(shift)
-time.sleep(0.3)
-PressKey(f10)
+time.sleep(0.5)
+PressKey(f10) # casting Energy Buffer
 ReleaseKey(f10)
-time.sleep(0.3)
+time.sleep(0.5)
 # PressKey(shift)
-time.sleep(0.3)
-PressKey(f9)
+time.sleep(0.5)
+PressKey(f9) # casting Improved Invisibility
 ReleaseKey(f9)
 pyautogui.click()
-time.sleep(0.3)
-PressKey(f8)
+time.sleep(0.5)
+pyautogui.moveTo(1225, 272) # move Mouse to familiar
+time.sleep(0.5) 
+PressKey(f8) # casting Stoneskin
 ReleaseKey(f8)
-pyautogui.moveTo(1225, 272) 
-time.sleep(0.3)
 pyautogui.click()
-time.sleep(0.3)
-PressKey(f6)
+time.sleep(0.5)
+PressKey(f6) # casting Greater Magic Weapon
 ReleaseKey(f6)
 pyautogui.click()
-time.sleep(0.3)
-PressKey(f5)
+time.sleep(0.5)
+PressKey(f5) #casting Flame Weapon
 ReleaseKey(f5)
 pyautogui.click()
-time.sleep(0.3)
-ReleaseKey(shift)
-
-time.sleep(0.3)
-
-PressKey(ctrl)
-PressKey(f11)
+time.sleep(0.5)
+ReleaseKey(shift) # Release Shift Key
+time.sleep(0.5)
+PressKey(ctrl) # Press Control Key
+time.sleep(0.5)
+PressKey(f11) # casting Endure Elements
 ReleaseKey(f11)
 pyautogui.click()
-ReleaseKey(ctrl)
-
-PressKey(shift)
-pyautogui.moveTo(1225, 80)
-time.sleep(0.3)
-PressKey(f6)
+ReleaseKey(ctrl) # Release Control Key
+time.sleep(0.5)
+pyautogui.moveTo(1225, 80) # Move Back to Charter
+time.sleep(0.5)
+PressKey(shift) # Press Shift Key
+time.sleep(0.5)
+PressKey(f6) # casting Greater Magic Weapon
 ReleaseKey(f6)
 pyautogui.click()
-time.sleep(0.3)
-PressKey(f5)
+time.sleep(0.5)
+PressKey(f5) # casting Flame Weapon
 ReleaseKey(f5)
 pyautogui.click()
-time.sleep(0.3)
-PressKey(f4)
+time.sleep(0.5)
+PressKey(f4) # casting Magic Circle against Evil
 ReleaseKey(f4)
 pyautogui.click()
-time.sleep(0.3)
-PressKey(f3)
+time.sleep(0.5)
+PressKey(f3) # casting Shield
 ReleaseKey(f3)
 # pyautogui.click()
-time.sleep(0.3)
-PressKey(f2)
+time.sleep(0.5)
+PressKey(f2) # casting Greater Stoneskin
 ReleaseKey(f2)
-time.sleep(0.3)
+time.sleep(0.5)
 
-ReleaseKey(shift)
+ReleaseKey(shift) # Release Shift Key
 
 # All Control Buffs Setup
-PressKey(ctrl)
+PressKey(ctrl) # Press Control Key
 
-time.sleep(0.3)
-PressKey(f10)
+time.sleep(0.5)
+PressKey(f10) # casting Premonition
 ReleaseKey(f10)
 # pyautogui.click()
-time.sleep(0.3)
-PressKey(f9)
+time.sleep(0.5)
+PressKey(f9) # casting Lesser Mind Blank
 ReleaseKey(f9)
 pyautogui.click()
-time.sleep(0.3)
-PressKey(f8)
+time.sleep(0.5)
+PressKey(f8) # casting Shadow Shield
 ReleaseKey(f8)
-time.sleep(0.3)
-PressKey(f7)
+time.sleep(0.5)
+PressKey(f7) # casting Spell Mantle
 ReleaseKey(f7)
-time.sleep(0.3)
-PressKey(f3)
+time.sleep(0.5)
+PressKey(f3) # casting True Seeing
 ReleaseKey(f3)
-time.sleep(0.3)
+time.sleep(0.5)
 pyautogui.click()
 
-ReleaseKey(ctrl)
+ReleaseKey(ctrl) # Release Control Key
 
 # End of Script!
 
